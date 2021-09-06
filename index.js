2d6c764643fc30eb4b02382f83f8c58f86fc8f64
Mayank pandey <mpandeymp0@gmail.com>
2021-09-06 18:58:14 +0530

    ..

--- index.js
+++ index.js
@@ -1,8 +1,10 @@
 const express = require("express");
 const app = express();
 const port = 3000;
-const connectToMongo=require("./db");
+const connectToMongo = require("./db");
 connectToMongo();
+
+app.use('/user',require("./routes/userRoute"));
 
 app.listen(port, () => {
   console.log(`App listening at http://localhost:${port}`);
---
+++ models/userModel.js
@@ -0,0 +1,11 @@
+const mongoose = require("mongoose");
+const { Schema } = mongoose;
+
+const userSchema = new Schema({
+  firstName: { type: String, required: true },
+  lastName: { type: String, required: true },
+  email: { type: String, required: true },
+  password: { type: String, required: true },
+  date: { type: Date, default: Date.now },
+});
+module.exports = mongoose.model("User", userSchema);
