04f89661920c6e4466b58a65864bc78773abac98
Mayank pandey <mpandeymp0@gmail.com>
2021-09-07 14:44:45 +0530

    ..

--- routes/userRoute.js
+++ routes/userRoute.js
@@ -5,9 +5,11 @@ const { body, validationResult } = require("express-validator");
 const bcrypt = require("bcryptjs");
 const jwt = require("jsonwebtoken");
 const JWT_SECRET = "Its Mayank";
+const fetchuser = require("../middleware/fetchUser");
 
+//Create a user ;
 router.post(
-  "/",
+  "/signup",
   [
     body("firstName", "Must be greater than 3 words").isLength({ min: 3 }),
     body("lastName").isLength({ min: 3 }),
@@ -40,12 +42,60 @@ router.post(
       };
       const authtoken = jwt.sign(data, JWT_SECRET);
       // console.log(jwtData);
-      res.json({authtoken});
+      res.json({ authtoken });
     } catch (error) {
       console.error(error.message);
       res.status(500).send("Error!");
     }
   }
 );
+
+//Login a user;
+
+router.post(
+  "/login",
+  [
+    body("email", "Enter a valid email").isEmail(),
+    body("password", "Password cannot be blank").exists(),
+  ],
+
+  async (req, res) => {
+    const errors = validationResult(req);
+    if (!errors.isEmpty()) {
+      return res.status(400).json({ errors: errors.array() });
+    }
+    const { email, password } = req.body;
+    try {
+      let user = await User.findOne({ email });
+      if (!user) {
+        return res.status(400).json({ error: "Invalid user credentials" });
+      }
+      const passwordCompare = await bcrypt.compare(password, user.password);
+      if (!passwordCompare) {
+        return res.status(400).json({ error: "Invalid user credentials" });
+      }
+      const data = {
+        user: { id: user.id },
+      };
+      const authtoken = jwt.sign(data, JWT_SECRET);
+      res.json({ authtoken });
+    } catch (error) {
+      console.error(error.message);
+      res.status(500).send("Invalid user ceredentials!");
+    }
+  }
+);
+//user details
+
+router.post("/getUser", fetchuser, async (req, res) => {
+  try {
+    userId = req.user.id;
+    const user = await User.findById(userId).select("-password");
+    res.send(user);
+  } catch (error) {
+    console.error(error.message);
+    res.status(500).send("Invalid user ceredentials!");
+  }
+});
 
 module.exports = router;
